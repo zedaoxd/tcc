f690444ad1b6822be54edf6238a39cb462af37b9
Bruno Lessa <brunolessa@live.com>
2024-05-15 18:52:27 -0300

    refactor: Update CurseDescription component to accept a className prop

--- frontend/src/components/modules/courses/components/cuse-description/index.tsx
+++ frontend/src/components/modules/courses/components/cuse-description/index.tsx
@@ -5,16 +5,21 @@ import { Jost } from "next/font/google";
 const jost = Jost({ subsets: ["latin"], weight: ["400"] });
 
 type Props = {
+  className?: string;
   description: string;
   Icon: LucideIcon;
 };
 
-export default function CurseDescription({ description, Icon }: Props) {
+export default function CurseDescription({
+  description,
+  Icon,
+  className,
+}: Props) {
   return (
     <span className="flex items-center gap-2">
       <Icon className="h-5 w-5 text-primary" />
 
-      <span className={cn(jost.className, "text-base leading-6")}>
+      <span className={cn(jost.className, "text-base leading-6", className)}>
         {description}
       </span>
     </span>
